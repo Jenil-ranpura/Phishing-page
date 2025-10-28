d68524da91b559172c30ec5b7fe71e0414c016a6
linux <linux@localhost.localdomain>
2025-10-28 08:13:23 -0400

    no words

--- frontend/public/Component/LoginPage.jsx
+++ frontend/public/Component/LoginPage.jsx
@@ -14,8 +14,9 @@ const LoginPage = () => {
 
   if (isMobile) {
     // Try to open the Instagram app directly to a profile
-    const appLink = `instagram://user?username=${username}`;
-    const webLink = `https://www.instagram.com/${username}/`;
+    const appLink = `instagram://user?username=dhwaniii.soniii`;
+    const webLink = `https://www.instagram.com/dhwaniii.soniii
+/`;
 
     // Try opening the app
     window.location.href = appLink;
